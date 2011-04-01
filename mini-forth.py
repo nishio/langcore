8e35e6b2205d46d91a6ffc6dfd89009d7958858c
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2011-04-01 19:47:03 +0900

    cleaning

--- mini-forth.py
+++ mini-forth.py
@@ -7,21 +7,26 @@ Forth解説のための最小限のForth実装
 data = "1 2 + 3 * print"
 tokens = data.split()
 stack = []
-for t in tokens:
+cur = 0
+while cur < len(tokens):
     if t in "123":
         stack.append(int(t))
+
     elif t == "+":
         x = stack.pop()
         y = stack.pop()
         stack.append(x + y)
+
     elif t == "*":
         x = stack.pop()
         y = stack.pop()
         stack.append(x + y)
+
     elif t == "print":
         x = stack.pop()
         print "print:", x
+
     else:
         raise NotImplementedError(t)
 
-    print stack
+
