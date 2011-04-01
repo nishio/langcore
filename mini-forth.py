a0884a5cf64906aa9b85f07675821b534fd8f722
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2011-04-01 19:24:20 +0900

    definable forth

--- mini-forth.py
+++ mini-forth.py
@@ -4,24 +4,51 @@ mini-Forth
 Forth解説のための最小限のForth実装
 """
 
-data = "1 2 + 3 * print"
+data = ": dp dup print ; 1 2 dp + 3 * print"
 tokens = data.split()
 stack = []
-for t in tokens:
-    if t in "123":
-        stack.append(int(t))
-    elif t == "+":
-        x = stack.pop()
-        y = stack.pop()
-        stack.append(x + y)
-    elif t == "*":
-        x = stack.pop()
-        y = stack.pop()
-        stack.append(x + y)
-    elif t == "print":
-        x = stack.pop()
-        print "print:", x
-    else:
-        raise NotImplementedError(t)
+words = {}
 
-    print stack
+def evaluate(tokens):
+    print "eval", tokens
+    cur = 0
+    while cur < len(tokens):
+        t = tokens[cur]
+        cur += 1
+        prevstack = stack[:]
+        if t in "123":
+            stack.append(int(t))
+        elif t == "+":
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(x + y)
+        elif t == "*":
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(x + y)
+        elif t == "print":
+            x = stack.pop()
+            print "print:", x
+        elif t == "dup":
+            x = stack.pop()
+            stack.append(x)
+            stack.append(x)
+        elif t == ":":
+            buf = []
+            while True:
+                t = tokens[cur]
+                cur += 1
+                if t == ";": break
+                buf.append(t)
+            words[buf[0]] = buf[1:]
+
+        elif words.has_key(t):
+            evaluate(words[t])
+        else:
+            raise NotImplementedError(t)
+
+        print "%s->(%s)->%s" % (prevstack, t, stack)
+
+
+evaluate(tokens)
+print words
