f84e516a009fc3c0d7f8a9d61aa2b5af1813ba30
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2011-04-01 19:45:23 +0900

    cleaning

--- mini-forth.py
+++ mini-forth.py
@@ -5,34 +5,38 @@ Forth解説のための最小限のForth実装
 """
 
 data = ": dp dup print ; 1 2 dp + 3 * print"
-tokens = data.split()
 stack = []
 words = {}
 
 def evaluate(tokens):
-    print "eval", tokens
     cur = 0
     while cur < len(tokens):
         t = tokens[cur]
         cur += 1
         prevstack = stack[:]
+
         if t in "123":
             stack.append(int(t))
+
         elif t == "+":
             x = stack.pop()
             y = stack.pop()
             stack.append(x + y)
+
         elif t == "*":
             x = stack.pop()
             y = stack.pop()
             stack.append(x + y)
+
         elif t == "print":
             x = stack.pop()
             print "print:", x
+
         elif t == "dup":
             x = stack.pop()
             stack.append(x)
             stack.append(x)
+
         elif t == ":":
             buf = []
             while True:
@@ -44,11 +48,14 @@ def evaluate(tokens):
 
         elif words.has_key(t):
             evaluate(words[t])
+
         else:
             raise NotImplementedError(t)
 
         print "%s->(%s)->%s" % (prevstack, t, stack)
 
 
+
+tokens = data.split()
 evaluate(tokens)
-print words
+
