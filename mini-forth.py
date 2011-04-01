abe5d13a623df15aadc688eed0c02e925e28663a
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2011-04-01 19:17:55 +0900

    initial mini-forth

---
+++ mini-forth.py
@@ -0,0 +1,27 @@
+# -*- enoding: utf-8 -*-
+"""
+mini-Forth
+Forth解説のための最小限のForth実装
+"""
+
+data = "1 2 + 3 * print"
+tokens = data.split()
+stack = []
+for t in tokens:
+    if t in "123":
+        stack.append(int(t))
+    elif t == "+":
+        x = stack.pop()
+        y = stack.pop()
+        stack.append(x + y)
+    elif t == "*":
+        x = stack.pop()
+        y = stack.pop()
+        stack.append(x + y)
+    elif t == "print":
+        x = stack.pop()
+        print "print:", x
+    else:
+        raise NotImplementedError(t)
+
+    print stack
